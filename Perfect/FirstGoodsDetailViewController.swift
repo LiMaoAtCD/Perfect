52c4e30375f66c8c1d2eb23aef1735f1d60e2266
liamscofield <limitscofield@gmail.com>
2016-05-29 23:47:37 +0800

    update

--- Perfect/FirstGoodsDetailViewController.swift
+++ Perfect/FirstGoodsDetailViewController.swift
@@ -8,12 +8,83 @@
 
 import UIKit
 
-class FirstGoodsDetailViewController: UIViewController {
+class FirstGoodsDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var compositionButton: UIButton!
+    var bottleButton: UIButton!
+    var boxButton: UIButton!
+    
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.translucent = false
+        tableView = UITableView.init(frame: view.bounds, style: .Plain)
+        view.addSubview(tableView)
+        tableView.snp_makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
+        let header = UIView.init(frame: CGRectMake(0, 0, Tools.width, 300))
+        tableView.tableHeaderView = header
+        
+        
+        
+        compositionButton = UIButton()
+        compositionButton.setTitle("组合", forState: .Normal)
+        compositionButton.backgroundColor = UIColor.brownColor()
+        header.addSubview(compositionButton)
+        
+        bottleButton = UIButton()
+        bottleButton.setTitle("酒瓶", forState: .Normal)
+        bottleButton.backgroundColor = UIColor.brownColor()
+
+        header.addSubview(bottleButton)
+        
+        boxButton = UIButton()
+        boxButton.setTitle("酒盒", forState: .Normal)
+        boxButton.backgroundColor = UIColor.brownColor()
+        header.addSubview(boxButton)
+        
+        compositionButton.snp_makeConstraints { (make) in
+            make.width.equalTo(50)
+            make.height.equalTo(35)
+            make.centerY.equalTo(view.snp_top).offset(50)
+            make.centerX.equalTo(view.snp_centerX).multipliedBy(0.5)
+        }
+        
+        bottleButton.snp_makeConstraints { (make) in
+            make.width.equalTo(50)
+            make.height.equalTo(35)
+            make.centerY.equalTo(view.snp_top).offset(50)
+            make.centerX.equalTo(view.snp_centerX).multipliedBy(1)
+        }
+        
+        boxButton.snp_makeConstraints { (make) in
+            make.width.equalTo(50)
+            make.height.equalTo(35)
+            make.centerY.equalTo(view.snp_top).offset(50)
+            make.centerX.equalTo(view.snp_centerX).multipliedBy(1.5)
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
     override func didReceiveMemoryWarning() {
